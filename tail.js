70b75cff70c26c79e21851d9d1ee51d88bacd4e9
Mahdi Teimouri <mahditei@mail.com>
2023-05-08 15:19:24 -0700

    <commit_message>

--- tail.js
+++ tail.js
@@ -1,27 +1,7 @@
-const assertEqual = function (actual, expected) {
-
-  let equal = false;
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} is equal to ${expected}`);
-    equal = true;
-  } else {
-    console.log(`Assertion Failed: ${actual} is not equal to ${expected}`);
-  }
-  return equal;
-};
-
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 11);
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual')
 
 const tail = function (arr) {
-  constOutArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== 0) {
-      outputArr.push(arr[i]);
-    }
-  }
-  return outputArr;
+  return arr.slice(1);
 };
+module.exports = tail;
+
