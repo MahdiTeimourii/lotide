e808de7f3e00dfc7b8a9afb4f8b0cb915e381f44
Mahdi Teimouri <mahditei@mail.com>
2023-05-08 16:42:58 -0700

    <commit_message>

--- middle.js
+++ middle.js
@@ -33,3 +33,5 @@ const middle = function (middleOfArr) {
 eqArrays([1, 2, 3], [1, 2, 3]);
 middle([1, 2, 3, 4]);
 middle([1, 2, 3, 4, 5, 6]);
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,5 @@
+const middle = require('./middle');
+const middleTest1 = require('./assertArraysEqual ');
+eqArrays([1, 2, 3], [1, 2, 3]);
+middle([1, 2, 3, 4]);
+middle([1, 2, 3, 4, 5, 6]);
