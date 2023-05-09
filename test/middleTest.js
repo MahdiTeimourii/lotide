84397186f5225cc2e9aebb3515a0b150411e7519
Mahdi Teimouri <mahditei@mail.com>
2023-05-08 18:17:25 -0700

    <commit_message>

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,10 @@
 const middle = require('./middle');
-const middleTest1 = require('./assertArraysEqual ');
+const assert = require('chai').assert;
+describe("#middle", () => {
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
 eqArrays([1, 2, 3], [1, 2, 3]);
 middle([1, 2, 3, 4]);
 middle([1, 2, 3, 4, 5, 6]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,11 @@
 const assertEqual = require('./tail');
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert;
+describe("#tail", () => {
+  it("returns ['There', 'Friend'] for ['Hello', 'There', 'Friend']", () => {
+    let testArray = ["Hello", "There", "Friend"];
+    assert.deepEqual(tail(testArray), ['There', 'Friend']);
+  });
+  assertEqual(head([5, 6, 7]), 5);
+  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-module.exports = tail;
+  module.exports = tail;
